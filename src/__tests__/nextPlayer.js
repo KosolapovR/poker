52c4eeb13231fd92fad922be02af28039c6f5d78
Kosolapov Roman <Kosolapov-r@bk.ru>
2020-05-16 15:45:32 +0300

    next Player test cases

---
+++ src/__tests__/nextPlayer.js
@@ -0,0 +1,205 @@
+import Game from '../server/game/game';
+import {Player} from "../server/game/player";
+import {GAME_STATUS_IN_GAME} from "../server/game/types";
+
+test('get next player on bb in firstRound, but-bet | sb-call | bb-call', () => {
+
+    const game = new Game();
+
+    game.setBank();
+    const bank = game.getBank();
+
+    const player1 = game.addPlayer({user: {name: "Роман1"}});
+    const player2 = game.addPlayer({user: {name: "Роман2"}});
+    const player3 = game.addPlayer({user: {name: "Роман3"}});
+
+    game.getBank().setBetValue(player1.postBigBlind(2));
+
+    game.setActivePlayer(player3);
+    game.playerBet(10);
+
+    game.setActivePlayer(player2);
+    game.playerCall();
+
+    game.setActivePlayer(player1);
+    game.playerCall();
+
+    let nextPlayer = game.getNextPlayer(player1);
+
+    expect(nextPlayer).toBeUndefined();
+});
+test('get next player on bb in firstRound, but-bet | sb-call | bb-raise', () => {
+
+    const game = new Game();
+
+    game.setBank();
+    const bank = game.getBank();
+
+    const player1 = game.addPlayer({user: {name: "Роман1"}});
+    const player2 = game.addPlayer({user: {name: "Роман2"}});
+    const player3 = game.addPlayer({user: {name: "Роман3"}});
+
+    game.getBank().setBetValue(player1.postBigBlind(2));
+
+    game.setActivePlayer(player3);
+    game.playerBet(10);
+
+    game.setActivePlayer(player2);
+    game.playerCall();
+
+    game.setActivePlayer(player1);
+    game.playerBet(20);
+
+    let nextPlayer = game.getNextPlayer(player1);
+
+    expect(nextPlayer).toBeInstanceOf(Player);
+    expect(nextPlayer.getPosition()).toBe('but');
+});
+test('get next player on bb in firstRound, but-fold | sb-call | bb-bet', () => {
+
+    const game = new Game();
+
+    game.setBank();
+    const bank = game.getBank();
+
+    const player1 = game.addPlayer({user: {name: "Роман1"}});
+    const player2 = game.addPlayer({user: {name: "Роман2"}});
+    const player3 = game.addPlayer({user: {name: "Роман3"}});
+
+    game.getBank().setBetValue(player1.postBigBlind(2));
+
+    game.setActivePlayer(player3);
+    game.playerFold();
+
+    game.setActivePlayer(player2);
+    game.playerCall();
+
+    game.setActivePlayer(player1);
+    game.playerBet(10);
+
+    let nextPlayer = game.getNextPlayer(player1);
+
+    expect(nextPlayer).toBeInstanceOf(Player);
+    expect(nextPlayer.getPosition()).toBe('sb');
+});
+
+test('get next player on bb in firstRound, but-call | sb-bet | bb-raise', () => {
+
+    const game = new Game();
+
+    game.setBank();
+    const bank = game.getBank();
+
+    const player1 = game.addPlayer({user: {name: "Роман1"}});
+    const player2 = game.addPlayer({user: {name: "Роман2"}});
+    const player3 = game.addPlayer({user: {name: "Роман3"}});
+
+    game.getBank().setBetValue(player1.postBigBlind(2));
+
+    game.setActivePlayer(player3);
+    game.playerCall();
+
+    game.setActivePlayer(player2);
+    game.playerBet(10);
+
+    game.setActivePlayer(player1);
+    game.playerBet(20);
+
+    let nextPlayer = game.getNextPlayer(player1);
+
+    expect(nextPlayer).toBeInstanceOf(Player);
+    expect(nextPlayer.getPosition()).toBe('but');
+});
+test('get next player on bb in firstRound, but-fold | sb-bet | bb-raise', () => {
+
+    const game = new Game();
+
+    game.setBank();
+    const bank = game.getBank();
+
+    const player1 = game.addPlayer({user: {name: "Роман1"}});
+    const player2 = game.addPlayer({user: {name: "Роман2"}});
+    const player3 = game.addPlayer({user: {name: "Роман3"}});
+
+    game.getBank().setBetValue(player1.postBigBlind(2));
+
+    game.setActivePlayer(player3);
+    game.playerFold();
+
+    game.setActivePlayer(player2);
+    game.playerBet(10);
+
+    game.setActivePlayer(player1);
+    game.playerBet(20);
+
+    let nextPlayer = game.getNextPlayer(player1);
+
+    expect(nextPlayer).toBeInstanceOf(Player);
+    expect(nextPlayer.getPosition()).toBe('sb');
+});
+
+test('get next player on bb in firstRound, all players limp', () => {
+
+    const game = new Game();
+
+    game.setBank();
+
+    const player1 = game.addPlayer({user: {name: "Роман1"}});
+    const player2 = game.addPlayer({user: {name: "Роман2"}});
+    const player3 = game.addPlayer({user: {name: "Роман3"}});
+
+    game.getBank().setBetValue(player1.postBigBlind(2));
+
+
+    game.setActivePlayer(player3);
+    game.playerCall();
+
+    game.setActivePlayer(player2);
+    game.playerCall();
+
+    game.setActivePlayer((player1));
+    game.playerCheck();
+
+    const nextPlayer = game.getNextPlayer(player1);
+
+    expect(nextPlayer).toBeUndefined();
+});
+test('get next player on btn in firstRound, all players limp', () => {
+
+    const game = new Game();
+
+    game.setBank();
+    const bank = game.getBank();
+
+    const player1 = game.addPlayer({user: {name: "Роман1"}});
+    const player2 = game.addPlayer({user: {name: "Роман2"}});
+    const player3 = game.addPlayer({user: {name: "Роман3"}});
+
+    game.getBank().setBetValue(player1.postBigBlind(2));
+
+    let nextPlayer = game.getNextPlayer(player3);
+
+    expect(nextPlayer).toBeInstanceOf(Player);
+    expect(nextPlayer.getPosition()).toBe('sb');
+});
+test('get next player on sb in firstRound, all players limp', () => {
+
+    const game = new Game();
+
+    game.setBank();
+    const bank = game.getBank();
+
+    const player1 = game.addPlayer({user: {name: "Роман1"}});
+    const player2 = game.addPlayer({user: {name: "Роман2"}});
+    const player3 = game.addPlayer({user: {name: "Роман3"}});
+
+    game.getBank().setBetValue(player1.postBigBlind(2));
+
+    let nextPlayer = game.getNextPlayer(player2);
+
+    expect(nextPlayer).toBeInstanceOf(Player);
+    expect(nextPlayer.getPosition()).toBe('bb');
+});
+
+
+
